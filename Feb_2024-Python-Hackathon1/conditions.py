680faaecdb3efd7c4d911d3acbe31433b9bcd225
ephraimejore <160174520+ephraimejore@users.noreply.github.com>
2024-03-19 03:22:30 +0300

    Add files via upload

---
+++ Feb_2024-Python-Hackathon1/conditions.py
@@ -0,0 +1,8 @@
+# Prompt user to enter their age
+age = int(input("Please enter your age: "))
+
+# Check if age is greater than or equal to 18
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
